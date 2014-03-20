606a50fe895b1fb7e50b03455c8dc25f49ef3788
Karlkim Suwamongkol <karlkim@outlook.com>
2014-03-19 21:28:13 -0500

    matchPattern

--- src/BingoEngine/BingoEngine/PatternMatcher.fs
+++ src/BingoEngine/BingoEngine/PatternMatcher.fs
@@ -1,8 +1,11 @@
 ﻿namespace BingoEngine
 
 module BingoPatterns =
-    let horizontalLines = 
-        [(0,0);(0,1);(0,1);(0,3);(0,4)]
+    let horizontalLines0 = [(0,0);(0,1);(0,2);(0,3);(0,4)] |> Set.ofList
+    let horizontalLines1 = [(1,0);(1,1);(1,2);(1,3);(1,4)] |> Set.ofList
+    let horizontalLines2 = [(2,0);(2,1);(2,2);(2,3);(2,4)] |> Set.ofList
+    let horizontalLines3 = [(3,0);(3,1);(3,2);(3,3);(3,4)] |> Set.ofList
+    let horizontalLines4 = [(4,0);(4,1);(4,2);(4,3);(4,4)] |> Set.ofList
 
 module PatternMatcher =
     open BingoCard
@@ -30,3 +33,35 @@ module PatternMatcher =
             |]
         | None -> card        
      
+    let matchPattern (card:Cell[][]) pattern =
+        let matchCell =
+            function
+            | Center | Called _ -> true
+            | _ -> false
+
+        let matchedCells = 
+            [
+                for row in 0..4 do
+                    for col in 0..4 do                                
+                        if matchCell card.[row].[col] then
+                            yield (row,col)                
+            ] |> Set.ofList
+
+        let inPatternCell =
+            function
+            | Called i -> InPattern i
+            | cell -> cell
+
+        if Set.isSubset pattern matchedCells then
+            Some [|
+                    for row in 0..4 ->
+                    [|
+                        for col in 0..4 ->
+                            if pattern |> Set.exists ((=)(row,col)) then
+                                inPatternCell card.[row].[col] 
+                            else
+                                card.[row].[col]
+                    |]
+                |]
+        else None
+             
